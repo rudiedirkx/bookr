67d44ca0fcfa94e00f00b7f19f9ea3dcf111508b
Rudie Dirkx <github@hotblocks.nl>
2022-09-14 02:17:19 +0200

    favicon II

--- tpl.header.php
+++ tpl.header.php
@@ -11,7 +11,7 @@ header('Content-type: text/html; charset=utf-8');
 	<meta name="theme-color" content="#333" />
 	<meta name="referrer" content="no-referrer" />
 	<title>Bookr</title>
-	<link rel="icon" type="image/x-icon" href="/favicon.ico" sizes="32x32" />
+	<link rel="icon" type="image/png" href="/favicon-128.png" sizes="128x128" />
 	<link rel="icon" href="/favicon.ico" type="image/x-icon" />
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
